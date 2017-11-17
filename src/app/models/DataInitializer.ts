e76f3268e4c190aaf1d5ea58fc8f6309c1bc95cc
billgiot <bill.giotofficial@gmail.com>
2017-11-17 20:39:42 +0100

    test4

--- src/app/models/DataInitializer.ts
+++ src/app/models/DataInitializer.ts
@@ -8,14 +8,14 @@ export class DataInitializer{
 
      makeButtonPerson():Array<Button> {
         let buttons:Array<Button> = new Array<Button>();  
-        buttons.push(new Button("don't fall asleep", "sound", "../../../assets/audio/dontfallasleep.wav")); 
-        buttons.push(new Button("penny thoughts", "sound", "../../../assets/audio/penny.wav")); 
-        buttons.push(new Button("treet", "sound", "../../../assets/audio/treet.wav")); 
-        buttons.push(new Button("Seagulls", "sound", "../../../assets/audio/song.mp3")); 
-        buttons.push(new Button("doit", "sound", "../../../assets/audio/doit.wav")); 
-        buttons.push(new Button("shrekislove", "sound", "../../../assets/audio/shrekislove.wav")); 
-        buttons.push(new Button("bushesoflove", "sound", "../../../assets/audio/bushesoflove.mp3")); 
-        buttons.push(new Button("kyle's mom", "sound", "../../../assets/audio/kylesmom.mp3")); 
+        buttons.push(new Button("don't fall asleep", "sound", "assets/audio/dontfallasleep.wav")); 
+        buttons.push(new Button("penny thoughts", "sound", "assets/audio/penny.wav")); 
+        buttons.push(new Button("treet", "sound", "assets/audio/treet.wav")); 
+        buttons.push(new Button("Seagulls", "sound", "assets/audio/song.mp3")); 
+        buttons.push(new Button("doit", "sound", "assets/audio/doit.wav")); 
+        buttons.push(new Button("shrekislove", "sound", "assets/audio/shrekislove.wav")); 
+        buttons.push(new Button("bushesoflove", "sound", "assets/audio/bushesoflove.mp3")); 
+        buttons.push(new Button("kyle's mom", "sound", "assets/audio/kylesmom.mp3")); 
         
         return buttons; 
     }
