1ae33f6df58feefa3f48ef7b45e3f4b284340751
billgiot <bill.giotofficial@gmail.com>
2017-12-12 11:49:51 +0100

    edit soundeffects

--- src/app/models/DataInitializer.ts
+++ src/app/models/DataInitializer.ts
@@ -8,14 +8,9 @@ export class DataInitializer{
 
      makeButtonPerson():Array<Button> {
         let buttons:Array<Button> = new Array<Button>();  
-        buttons.push(new Button("don't fall asleep", "sound", "assets/audio/dontfallasleep.wav")); 
-        buttons.push(new Button("penny thoughts", "sound", "assets/audio/penny.wav")); 
-        buttons.push(new Button("doit", "sound", "assets/audio/doit.wav")); 
+        buttons.push(new Button("truckhorn", "sound", "assets/audio/truckhorn.mp3")); 
+        buttons.push(new Button("airhorn", "sound", "assets/audio/airhorn.mp3"));                 
         buttons.push(new Button("doh", "sound", "assets/audio/doh.mp3")); 
-        buttons.push(new Button("Le Bron James", "sound", "assets/audio/lebronjames.mp3")); 
-        buttons.push(new Button("The ting go skrrra pap pap ka ka ka", "sound", "assets/audio/skrrra.mp3")); 
-        buttons.push(new Button("Dramatic Squirrel", "sound", "assets/audio/dramaticSquirrel.mp3")); 
-        buttons.push(new Button("Goat Yelling", "sound", "assets/audio/goatYelling.wav")); 
         
         return buttons; 
     }
